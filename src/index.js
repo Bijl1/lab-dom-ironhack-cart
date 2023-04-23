6221a37dbe8eebcc140bb48d636ff3143ca50ad7
Jonathan Bijl <sannoayo@gmail.com>
2023-04-23 18:36:55 -0400

    First commit of the day

--- src/index.js
+++ src/index.js
@@ -2,15 +2,25 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span').innerText;
+  const quantity = product.querySelector('.quantity input').value;
+  const subtotal = price * quantity;
+  product.querySelector('.subtotal span').innerText = subtotal.toFixed(2);
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  function calculateAll() {
+    const products = document.querySelectorAll('.product');
+    let total = 0;
+    products.forEach(product => {
+      total += updateSubtotal(product);
+    });
+    document.querySelector('#total-value span').innerText = total.toFixed(2);
+  }
+  
   // end of test
 
   // ITERATION 2
@@ -23,9 +33,12 @@ function calculateAll() {
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  const target = event.currentTarget;
+  const product = target.parentNode.parentNode;
+  product.parentNode.removeChild(product);
+  calculateAll();
+  
 }
 
 // ITERATION 5
